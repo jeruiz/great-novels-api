cab1215dba9533bed458c947e9913a595353cb4f
Jessica Ruiz <jeruiz1010@gmail.com>
2021-01-11 16:01:02 -0500

    error ith index.js and controller/authors that will not allow the port to listen

--- models/index.js
+++ models/index.js
@@ -5,14 +5,23 @@ const NovelsModel = require('./novels')
 const NovelsGenresModel = require('./novelsGenres')
 
 const connection = new Sequelize('novels', 'novels', 'Gre@tnov3ls', {
-    host: 'localhost', dialect: 'mysql',
+  host: 'localhost', dialect: 'mysql',
 })
 
-const Authors = AuthorsModel( connection, Sequelize)
-const Genres = GenresModel( connection, Sequelize)
-const Novels = NovelsModel( connection, Sequelize, Authors)
-const NovelsGenres = NovelsGenresModel( connection, Sequelize, Genres, Novels)
+const Authors = AuthorsModel(connection, Sequelize)
+const Genres = GenresModel(connection, Sequelize)
+const Novels = NovelsModel(connection, Sequelize, Authors)
+const NovelsGenres = NovelsGenresModel(connection, Sequelize, Genres, Novels)
 
 Novels.belongsTo(Authors)
 Authors.belongsTo(Novels)
 
+Genres.belongsToMany(Novels, { through: NovelsGenres })
+Genres.belongsToMany(Genres, { through: NovelsGenres })
+
+module.exports = {
+  Authors,
+  Genres,
+  Novels,
+  NovelsGenres,
+}
--- models/novelsGenres.js
+++ models/novelsGenres.js
@@ -1,8 +1,8 @@
-const NovelsGenres = (connection, Sequelize, Genres, Novels ) => {
-    return connection.define('novelsGenres', {
-        genreId: { type: Sequelize.INTEGER, primaryKey: true, reference: { model: Genres, key: 'id'} },
-        novelId: { type: Sequelize.INTEGER, primaryKey: true, reference: { model: Novels, key: 'id'} },
-    })
+const NovelsGenres = (connection, Sequelize, Genres, Novels) => {
+  return connection.define('novelsGenres', {
+    genreId: { type: Sequelize.INTEGER, primaryKey: true, reference: { model: Genres, key: 'id' } },
+    novelId: { type: Sequelize.INTEGER, primaryKey: true, reference: { model: Novels, key: 'id' } },
+  })
 }
 
 module.exports = NovelsGenres
